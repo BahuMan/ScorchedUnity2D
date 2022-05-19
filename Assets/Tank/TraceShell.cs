899669ea307fdba83e13f9bb5dc4ba4aeda52f1a
Bart Huylebroeck <bart.huylebroeck@pandora.be>
2022-05-19 15:02:39 +0200

    fix trace when projectile wraps around screen

--- Assets/Tank/TraceShell.cs
+++ Assets/Tank/TraceShell.cs
@@ -9,6 +9,7 @@ public class TraceShell : MonoBehaviour
     
     private Transform _projectile;
     private float _nextStep;
+    private GenericPlayer _player;
 
     public void Trace(Transform theProjectile, GenericPlayer pl)
     {
@@ -20,6 +21,7 @@ public class TraceShell : MonoBehaviour
         myLine.SetPosition(1, theProjectile.position);
         myLine.startColor = pl.PlayerColor - new Color(0,0,0,.5f);
         myLine.endColor = pl.PlayerColor - new Color(0, 0, 0, .5f);
+        _player = pl;
     }
 
     // Update is called once per frame
@@ -32,10 +34,22 @@ public class TraceShell : MonoBehaviour
             return;
         }
 
+        //if the x-position changed sign, the projectile wrapped around
+        //and we need to split the trace in 2
+        if ((_projectile.position.x * myLine.GetPosition(myLine.positionCount - 1).x) < 0)
+        {
+            Debug.Log("splitting trace");
+            TraceShell newTrace = Instantiate<TraceShell>(this);
+            newTrace.Trace(_projectile, _player);
+            Destroy(this);
+            return;
+        }
+
         if (Time.time > _nextStep)
         {
             myLine.positionCount++;
         }
+
         myLine.SetPosition(myLine.positionCount - 1, _projectile.position);
     }
 
